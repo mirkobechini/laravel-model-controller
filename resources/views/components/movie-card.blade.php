0909376266e9505ed4c311f9d5dbae4aa96d5f21
mirkobechini <mirkobechini@gmail.com>
2026-01-03 16:01:09 +0100

    Style: added vote stars

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,11 +1,22 @@
-<div class="card" >
+<div class="card">
     <div class="card-body">
         <h5 class="card-title">{{$movie["title"]}}</h5>
         <h6 class="card-subtitle mb-2 text-body-secondary">({{$movie["original_title"]}})</h6>
-        <p class="card-text">{{$movie["nationality"]}}</p>
-        <span class="card-text d-block">{{$movie["date"]}}</span>
+        <p class="card-text text-body-secondary">{{$movie["nationality"]}}</p>
+        <span class="card-text text-body-secondary d-block">{{$movie["date"]}}</span>
     </div>
-    <div class="card-footer bg-primary-subtle text-primary">
-        <span class="card-text d-block">votestars(numero)</span>
+    <div class="card-footer bg-primary-subtle text-primary border-top border-dark">
+        <span class="card-text d-block">
+            @for ($i=0; $i<10; $i++)
+                @if ($movie["vote"]>= $i)
+                <i class="bi bi-star-fill"></i>
+                @elseif ($movie["vote"] >= $i - 0.5)
+                <i class="bi bi-star-half"></i>
+                @else
+                <i class="bi bi-star"></i>
+                @endif
+            @endfor
+        ({{$movie["vote"]}})
+    </span>
     </div>
 </div>
